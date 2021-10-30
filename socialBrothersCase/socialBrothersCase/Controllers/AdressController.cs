85f3df07cbc4aa372964f518ced483ff0c5b6a37
Wouter van Mierlo <woutervanmierlo28@outlook.com>
2021-10-30 13:12:57 +0200

    db can now be accessed from AdressController, added get methods

--- socialBrothersCase/socialBrothersCase/Controllers/AdressController.cs
+++ socialBrothersCase/socialBrothersCase/Controllers/AdressController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using socialBrothersCase.DatabaseContexts;
+using socialBrothersCase.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,24 +16,33 @@ namespace socialBrothersCase.Controllers
     [ApiController]
     public class AdressController : ControllerBase
     {
+        private AdressesContext _adressesContext;
+        public AdressController(AdressesContext adressesContext)
+        {
+            _adressesContext = adressesContext;
+
+        }
+
         // GET: api/<AdressController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Adress> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _adressesContext.Adresses.ToList();
         }
 
         // GET api/<AdressController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Adress Get(Guid id)
         {
-            return "value";
+            //TODO add catch if there is no adress with given ID
+            return _adressesContext.Adresses.Where(a => a.Id == id).First();
         }
 
         // POST api/<AdressController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public string Post([FromBody] Adress value)
         {
+            return "test";
         }
 
         // PUT api/<AdressController>/5
